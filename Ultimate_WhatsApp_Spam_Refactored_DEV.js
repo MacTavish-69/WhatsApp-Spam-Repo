71e7df2c056ffad5066a2a05b3e20f01c56f8d05
Mac <tehseenhashmi10@gmail.com>
2020-06-10 15:34:31 +0500

    Added Sticker Spam Feature
    Added a Dev file and minified file to use in console.

--- Ultimate_WhatsApp_Spam_Refactored_DEV.js
+++ Ultimate_WhatsApp_Spam_Refactored_DEV.js
@@ -1,14 +1,13 @@
 /* Here is actual script to paste into console */
 // Written By MacTavish. Discord Tag: MacTavish#8517
+
 function createForm() {
+	
   // Create a style tag to host style rules
 	let body = document.getElementById("app");
 	let style = document.createElement('style');
 	style.type = 'text/css';
 	style.innerHTML = `
-    * {
-      // color: silver;
-    }
 	  .hide-me {
       display: none !important;
     }
@@ -27,15 +26,15 @@ function createForm() {
       justify-content: center;
     }
 	 .text-input {
-    display: block;
-    width: 80%;
-    padding: .5rem;
-    margin: .5rem;
-    margin-bottom: 1rem;
-    border: none;
-    border-bottom: 1px solid silver;
-    color: silver;
-    background-color: #404040;
+	    display: block;
+	    width: 80%;
+	    padding: .5rem;
+	    margin: .5rem;
+	    margin-bottom: 1rem;
+	    border: none;
+	    border-bottom: 1px solid silver;
+	    color: silver;
+	    background-color: #404040;
     }
     .spam-btns {
       display: block;
@@ -55,9 +54,9 @@ function createForm() {
       font-weight: 600;
     }
     .radio-input-wrapper {
-    color: silver;
-    padding: 5px;
-    margin: 5px;
+	    color: silver;
+	    padding: 5px;
+	    margin: 5px;
     }
     .delete {
       margin-top: 5px;
@@ -118,7 +117,7 @@ function createForm() {
 	form.innerHTML = `
 	  <span class="radio-input-wrapper">
       <label for="sticker-checkbox">Send a Sticker?</label>
-      <input id="sticker-checkbox" class="radio-input" type="checkbox" name="sticker_checkbox" onclick="toggleDisplay()">
+      <input id="sticker-checkbox" class="radio-input" type="checkbox" name="toggle_checkbox" onclick="toggleDisplay()">
     </span>
 
     <div class="sticker-wrapper">
@@ -143,7 +142,7 @@ function createForm() {
     </div>
 
     <input id="counter" class="text-input" id="message" type="number" name="counter" placeholder="For how many times?" required>
-    <button id="fire" class="spam-btns" type="button" name="spam-btn" onclick="spam()">Spam it !</button>
+    <button id="fire-text-msg" class="spam-btns" type="button" name="spam-btn" onclick="spam()">Spam it !</button>
     `;
 	body.appendChild(form);
 }
@@ -153,6 +152,9 @@ createForm();
    * A place for GLOBAL VARIABLES. DO NOT MUTATE! .
   */
   var form = document.getElementById("form");
+  var toggle_checkbox = document.getElementById("sticker-checkbox");
+	var text_msg_form_container = document.getElementById("text-msg-form-container");
+	var text_msg_spam_btn = document.getElementById("fire-text-msg");
   var count = document.getElementById("counter");  
 
   // Get the container to append the messge to 
@@ -235,6 +237,63 @@ function spam() {
     */
     document.querySelector("._1U1xa").click();
   }
+}
+
+function stickerSpam() {
+
+		// Get the length of all the sent sticker Array
+		let stickers_length = document.querySelectorAll("._11S5R").length;
+
+		// The last sent sticker is the one we need
+		let sent_sticker = document.querySelectorAll("._11S5R")[stickers_length-1];
+		let sent_sticker_SRC = sent_sticker.getAttribute("src");
+
+		// Get the smiley button so we can navigate precisely to sticker
+		let smiley = document.querySelector("._2Q56Q");
+		// Open the smiley button so we can get stickers menu
+		smiley.click();
+
+		// Get the desired sticker using SRC of sent sticker
+		// Refer to https://stackoverflow.com/questions/37081721/use-variables-in-document-queryselector for CSS.escape
+		let sticker = document.querySelectorAll("img[src=" +CSS.escape(sent_sticker_SRC)+ "]");
+		let sticker_length = sticker.length;
+
+		// Get the number of times to spam sticker
+		let counter = count.value;
+
+		for (let i = 0; i < counter; i++) {
+			sticker[sticker_length-1].click();
+		}
+
+}
+
+function toggleDisplay () {
+
+	if (toggle_checkbox.checked === false) {
+		text_msg_form_container.classList.remove("hide-me");
+		text_msg_spam_btn.classList.remove("hide-me");
+
+		// Hide the button for Sticker Spam
+		document.getElementById("sticker-MRLS").classList.add("hide-me");
+	}
+	if (toggle_checkbox.checked === true) {
+		text_msg_form_container.classList.add("hide-me");
+		text_msg_spam_btn.classList.add("hide-me");
+
+		// create a Sticker Spam Fire button
+		if (document.getElementById("sticker-MRLS") === null) {
+			let sticker_spam_btn = document.createElement("div");
+			sticker_spam_btn.id = "sticker_spam_btn-wrapper";
+			sticker_spam_btn.innerHTML = `
+			<button id="sticker-MRLS" class="spam-btns"
+				type="button" name="spam-btn" onclick="stickerSpam()">Spam Sticker !</button>
+			`
+			// Append to the form
+			let form = document.getElementById("form");
+			form.appendChild(sticker_spam_btn);
+		}
+		document.getElementById("sticker-MRLS").classList.remove("hide-me");
+	}
 }
 
 function delete_form() {
